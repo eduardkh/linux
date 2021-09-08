863fe87bbd6b2b01b29b1bb210f4d9a327d50623
Eduard Khiaev <eddietheone1984@gmail.com>
2021-09-08 12:51:09 +0300

    install zsh with omz

---
+++ zsh/README.md
@@ -0,0 +1,6 @@
+### install zsh with omz
+```
+nano zsh_install.sh
+chmod +x zsh_install.sh
+./zsh_install.sh
+```
---
+++ zsh/zsh_install.sh
@@ -0,0 +1,18 @@
+# install zsh
+sudo sudo apt install zsh -y
+
+# install plugins
+sudo sudo apt install zsh-autosuggestions -y
+sudo sudo apt install zsh-syntax-highlighting -y
+
+# install omz
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# add plugins (activate)
+sed -i 's/plugins=(git)/plugins=( git kubectl sudo)/g' .zshrc
+echo "source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> .zshrc
+echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> .zshrc
+echo "alias k=kubectl" >> .zshrc
+# switch to zsh
+chsh -s $(which zsh)
+zsh
