9a6d9430e410b1d3e7f7a9c0e2b7befbb4a8d27c
Eduard Khiaev <eddietheone1984@gmail.com>
2021-09-24 20:56:25 +0300

    install golang-zsh

---
+++ golang-zsh/README.md
@@ -0,0 +1,8 @@
+### install golang-zsh
+
+> install
+
+```
+git clone https://github.com/eduardkh/linux.git
+bash linux/golang-zsh/golang-zsh_install.sh
+```
---
+++ golang-zsh/golang-zsh_install.sh
@@ -0,0 +1,12 @@
+# install golang (manual)
+wget https://golang.org/dl/go1.17.1.linux-amd64.tar.gz
+sudo sh -c 'rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.1.linux-amd64.tar.gz'
+echo 'export PATH=$PATH:/usr/local/go/bin:~/go/bin' >> ~/.zshrc
+
+# add Completion to golang
+go get -u github.com/posener/complete/gocomplete
+gocomplete -install -y
+
+# install gofiber
+go get -u github.com/gofiber/fiber/v2
+go get -u github.com/gofiber/cli/fiber
