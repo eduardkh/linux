5221d6d34a1b2ab790f27d54199f3f9c87a3c063
Eduard Khiaev <eddietheone1984@gmail.com>
2022-03-21 21:17:05 +0200

    update golang-zsh_install

--- golang-zsh/golang-zsh_install.sh
+++ golang-zsh/golang-zsh_install.sh
@@ -1,12 +1,14 @@
 # install golang (manual)
-wget https://golang.org/dl/go1.17.1.linux-amd64.tar.gz
-sudo sh -c 'rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.1.linux-amd64.tar.gz'
-echo 'export PATH=$PATH:/usr/local/go/bin:~/go/bin' >> ~/.zshrc
+curl -OL $(echo -n "https://go.dev" ; curl https://go.dev/dl/ | grep "download.*downloadBox" | grep -io "/dl/.*$(uname -s).*gz")
+sudo sh -c 'rm -rf /usr/local/go && tar -C /usr/local -xzf $(ls | grep -io "go.*.*gz")'
+echo 'export PATH=$PATH:/usr/local/go/bin:~/go/bin/' >> ~/.zshrc
 
 # add Completion to golang
-sudo sh -c 'go get -u github.com/posener/complete/gocomplete'
-sudo sh -c 'gocomplete -install -y'
+# sudo sh -c 'go install github.com/posener/complete/gocomplete@latest' # older version
+# sudo sh -c 'gocomplete -install -y' # older version
+go install github.com/posener/complete/gocomplete@latest
+gocomplete -install -y
 
 # install gofiber
-go get -u github.com/gofiber/fiber/v2
-go get -u github.com/gofiber/cli/fiber
+go install github.com/gofiber/fiber/v2@latest
+go install github.com/gofiber/cli/fiber@latest
