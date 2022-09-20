b580859d77cfdc20f5491f9d81c2dd2bfd52268b
Eduard Khiaev <eddietheone1984@gmail.com>
2022-09-20 15:20:36 +0300

    helm_completion_zsh

--- go cobra completion/crane/commands.sh
+++ go cobra completion/crane/commands.sh
@@ -1,7 +1,7 @@
 # install crane (with go)
 go install github.com/google/go-containerregistry/cmd/crane@latest
 
-# get auto-completion for crane in zsh
+# get auto-completion for crane in zsh (https://helm.sh/docs/helm/helm_completion_zsh/)
 crane completion zsh > "${fpath[1]}/_crane"
 
 # download an image to local dir
