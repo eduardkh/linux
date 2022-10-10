3f368ca08d60ac54fc65fb582966da95a72a067d
Eduard Khiaev <38815230+eduardkh@users.noreply.github.com>
2022-10-10 20:10:46 +0300

    Update zsh_install.sh

--- zsh/zsh_install.sh
+++ zsh/zsh_install.sh
@@ -9,10 +9,10 @@ sudo apt install zsh-syntax-highlighting -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # add plugins (activate)
-sed -i 's/plugins=(git)/plugins=( git kubectl sudo)/g' .zshrc
-echo "source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> .zshrc
-echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> .zshrc
-echo "alias k=kubectl" >> .zshrc
+sed -i 's/plugins=(git)/plugins=( git kubectl sudo)/g' ~/.zshrc
+echo "source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+echo "alias k=kubectl" >> ~/.zshrc
 # switch to zsh
 chsh -s $(which zsh)
 zsh
