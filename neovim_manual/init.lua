b3ba966686b498cd0b3a6dbb7712bf190226857b
Eduard Khiaev <eddietheone1984@gmail.com>
2023-12-11 14:45:25 +0200

    telescope

--- neovim_manual/init.lua
+++ neovim_manual/init.lua
@@ -1,13 +1,13 @@
 -- Basic Neovim settings
-vim.cmd('set expandtab')
-vim.cmd('set tabstop=2')
-vim.cmd('set softtabstop=2')
-vim.cmd('set shiftwidth=2')
+vim.cmd('set expandtab') -- Enable the use of spaces instead of tabs
+vim.cmd('set tabstop=2') -- Number of spaces that a <Tab> in the file counts for
+vim.cmd('set softtabstop=2') -- Number of spaces that a <Tab> counts for while performing editing operations
+vim.cmd('set shiftwidth=2') -- Size of an 'indent'
 
--- Path for lazy loader
+-- Path for the lazy loader
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
--- Clone lazy.nvim if it doesn't exist
+-- Clone the lazy.nvim plugin if it doesn't exist
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
         "git", "clone", "--filter=blob:none",
@@ -16,15 +16,28 @@ if not vim.loop.fs_stat(lazypath) then
     })
 end
 
--- Prepend the runtime path
+-- Prepend the runtime path with the lazy loader path
 vim.opt.rtp:prepend(lazypath)
 
 -- Plugin configuration
-local plugins = {{"catppuccin/nvim", name = "catppuccin", priority = 1000}}
+local plugins = {
+    {"catppuccin/nvim", name = "catppuccin", priority = 1000}, -- Catppuccin theme plugin
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.5', -- Specific version tag
+        dependencies = {'nvim-lua/plenary.nvim'} -- Dependency for telescope
+    }
+}
 
-local opts = {}
+local opts = {} -- Options for the lazy loader, empty if default
 
--- Setup lazy loader
+-- Setup the lazy loader with the defined plugins
 require("lazy").setup(plugins, opts)
-require("catppuccin").setup()
-vim.cmd("colorscheme catppuccin")
+
+-- Telescope setup
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<C-p>', builtin.find_files, {}) -- Key mapping for telescope find_files function
+
+-- Catppuccin theme setup and application
+require("catppuccin").setup() -- Setup Catppuccin theme
+vim.cmd("colorscheme catppuccin") -- Apply Catppuccin colorscheme
